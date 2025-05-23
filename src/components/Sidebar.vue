d65ae8b0d1c0b476e803466e0bb8e71ce8bed75f
Tane4ka170 <tane4ka170@gmail.com>
2025-05-23 22:27:00 +0300

    Feat: created notes sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <aside class="w-[256px] min-h-screen bg-gray-800">
+    <div class="p-4">
+      <button class="button w-full">New Note</button>
+    </div>
+  </aside>
+</template>
